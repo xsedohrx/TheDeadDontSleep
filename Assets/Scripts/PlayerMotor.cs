cc46c4687231f8fca04793b07e125ab68baa7502
Tim Pickup <tim.pickup@fizzcube.co.uk>
2022-05-23 22:29:32 +0100

    movement and rotation in 3d

--- Assets/Scripts/PlayerMotor.cs
+++ Assets/Scripts/PlayerMotor.cs
@@ -18,7 +18,6 @@ public class PlayerMotor : MonoBehaviour
 
     public static float Angle(Vector2 vector2)
     {
-        vector2.x *= -1;
         if (vector2.x < 0)
         {
             return 360 - (Mathf.Atan2(vector2.x, vector2.y) * Mathf.Rad2Deg * -1);
@@ -32,7 +31,7 @@ public class PlayerMotor : MonoBehaviour
     private void MoveTo(Vector2 direction)
     {
         var angleMoving = Angle(direction);
-        var angleLooking = transform.rotation.eulerAngles.z;
+        var angleLooking = transform.rotation.eulerAngles.y;
         var angleDifference = ((angleMoving - angleLooking) % 360);
         if (angleDifference < 0)
         {
@@ -45,9 +44,9 @@ public class PlayerMotor : MonoBehaviour
         angleDifference = Math.Abs(angleDifference);
         //angleDifference now in the range 0 to 180
         //ok we slow the player down up to 50% if full backwards and 100% if forwards.. 
-        var speedDirectionMultiplier = Mathf.Clamp(0.5f + (0.5f * (180 - angleDifference)/180), 0.5f, 1); ;
+        var speedDirectionMultiplier = Mathf.Clamp(0.5f + (0.7f * (180 - angleDifference)/180), 0.3f, 1); ;
 
-        transform.position += new Vector3( direction.x, direction.y )  * movementSpeed * speedDirectionMultiplier * Time.deltaTime;
+        transform.position += new Vector3( direction.x, 0, direction.y )  * movementSpeed * speedDirectionMultiplier * Time.deltaTime;
 
         Debug.LogWarning(angleMoving + " / " + angleLooking + " / " + angleDifference + " / " + speedDirectionMultiplier );
     }
